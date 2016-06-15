2aadd60146bfac23f6bd5994ead36651c3788d7a
Dominique Bashizi <dominique@blupods.com>
2016-06-15 11:18:44 -0400

    Cleaning up unused code

--- md-editor.js
+++ md-editor.js
@@ -1,30 +1,11 @@
 angular.module('MDEditorApp', [])
    .controller('EditorController', function($scope, $http, $sce) {
 
-   		$scope.testSomething = function() {
-   			console.log("testSomething()");
-   			$scope.name = "James";
-
-            if (false) {
-                $http.get("/games.json")
-                    .then(
-                        function successCallback(response) {
-                            console.log(response.data);
-                            console.log("Adding data to scope");
-                            $scope.games = response.data;
-                        },
-                        function errorCallback(response) {
-                            console.log("Unable to get data");
-                        });
-            }
-   		};
-
       $scope.renderText = function() { 
         $scope.formattedText = $sce.trustAsHtml($scope.md.render($scope.testContent));
       }
 
-   		console.log("EditorController ...");
-    	$scope.name = "James";
+      console.log("Initializing EditorController ...");
 
       $scope.testContent = "## Some initial value for the text content of the text area \n";
       $scope.testContent += "### Here is a smaller heading\n"; 
@@ -32,11 +13,9 @@ angular.module('MDEditorApp', [])
       $scope.testContent += "* Item 1\n"; 
       $scope.testContent += "* Item 2\n"; 
       $scope.testContent += "* Item 3\n"; 
-      $scope.md = window.markdownit();
-      // $scope.formattedText = md.render('# markdown-it rulezz!');
-      $scope.formattedText = $sce.trustAsHtml($scope.md.render($scope.testContent));
-      // alert(formattedText); 
 
-    	console.log("Initializing EditorController ...");
+      $scope.md = window.markdownit();
+      $scope.formattedText = $sce.trustAsHtml($scope.md.render($scope.testContent));
+
 	});
 
